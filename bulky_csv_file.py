69c44f324ba3158de8414ee36b7af5bf5c9c3a8a
Awan <108222291+cloudsy-na@users.noreply.github.com>
2022-09-03 16:01:27 +0700

    commit_1

---
+++ bulky_csv_file.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import googletrans
+from googletrans import Translator, LANGUAGES
+import ctypes
+
+df = pd.read_csv ("C:\\Users\\andriawan\\Documents\\2022\\Python\Latihan_2\\translate\\bq-results-re003.csv")
+#print(df.head(5))
+
+translator = Translator()
+translations = {}
+for column in df.columns:
+    # Unique elements of the column
+    unique_elements = df['pg_name'].unique()
+    for element in unique_elements:
+        # Adding all the translations to a dictionary (translations)
+        translations[element] = translator.translate(element, dest='id').text 
+#print(translations)
+
+df_trans = df.replace(translations, inplace = True)
+#print(df.head(20))
+
+#df.to_excel("filename_here.xlsx")
+df.to_csv("bq-results-re003_output.csv")
+
+ctypes.windll.user32.MessageBoxW(0, "Selamat Bro, Berhasil !","Sekilas Info", 0)
